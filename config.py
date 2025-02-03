8390085f1aba8e96a3241a10dab5a56da8e43a3a
Hori_lab_analysis_PC <utaro.hori@gmail.com>
2025-02-03 18:15:41 +0900

    Added config

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ RDNA_REF_HUMAN = 'references/bwa/human_rDNA.fa'
 RDNA_REF_HUMAN_COD = 'references/bwa/human_rDNA_only_coding.fa'
 RDNA_REF_HUMAN_SHIFTED = 'references/bwa/shifed_hrDNA.fa'
 RDNA_REF_MOUSE = 'references/bwa/mouse_vlad1.fa'
+SPLIT_LENGTH = 300
