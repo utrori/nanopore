5b9507da284fd823732ad64e1810f2c331068f64
Hori_lab_analysis_PC <utaro.hori@gmail.com>
2025-02-05 20:34:48 +0900

    dorado_io_completed

--- dorado_bam_io.py
+++ dorado_bam_io.py
@@ -1,4 +1,5 @@
 import pysam
+import matplotlib.pyplot as plt
 import numpy as np
 from dataclasses import dataclass, field
 from typing import List, Dict, Tuple, Optional
@@ -10,10 +11,11 @@ class AlignmentInfo:
     reference_end: int
     query_alignment_start: int
     query_alignment_end: int
+    length: int
     cigar: str
     mapping_quality: int
     flag: int
-    methylation_data: List[Tuple[str, list]] = field(default_factory=list)
+    methylation_data: List[Tuple[int, int]] = field(default_factory=list)
 
 @dataclass
 class ReadData:
@@ -31,57 +33,95 @@ class BamAnalyzer:
         self.dorado_bam = None
 
     def __enter__(self):
-        self.mapped_bam = pysam.AlignmentFile(self.mapped_bam_path, "rb")
-        self.dorado_bam = pysam.AlignmentFile(self.dorado_bam_path, "rb")
+        self.dorado_bam = pysam.AlignmentFile(self.dorado_bam_path, "rb", check_sq=False)
+        self.mapped_bam = pysam.AlignmentFile(self.mapped_bam_path, "rb", check_sq=False)
+        self.methylation_data_by_read_id = self._load_methylation_data()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.mapped_bam.close()
         self.dorado_bam.close()
 
-    def _extract_methylation_data(self, read_id: str) -> List[Tuple[str, list]]:
-        """Extracts methylation data for a given read ID from the Dorado BAM."""
-        methylation_data = []
-        for read in self.dorado_bam.fetch(until_eof=True):
-            if read.query_name == read_id:
-                # MM tag for modified bases, separated by bases
-                meth_tag = read.get_tag('MM') if read.has_tag('MM') else ''
-                # ML tag for methylation calls (probabilities)
-                ml_tag = read.get_tag('ML') if read.has_tag('ML') else []
-                methylation_data.append((meth_tag, ml_tag))
-        return methylation_data
+    def _load_methylation_data(self) -> Dict[str, List[Tuple[int, int]]]:
+        """Loads CpG methylation data from the Dorado BAM into a dictionary."""
+        methylation_data_by_read_id = {}
+        
+        for read in self.dorado_bam:
+            read_id = read.query_name
+            if read.has_tag('MM') or read.has_tag('Ml'):
+                modified_bases = read.modified_bases
+                if ('C', 0, 'm') in modified_bases:
+                    methylation_data_by_read_id[read_id] = read.modified_bases[('C', 0, 'm')]
+
+        return methylation_data_by_read_id
 
     def analyze(self) -> Dict[str, ReadData]:
         """Analyzes multiply-aligned reads, extracting methylation data and organizing by read ID."""
         reads_data = {}
 
         for read in self.mapped_bam:
-            if not read.is_secondary:  # Consider only secondary alignments (multiply-mapped reads)
-                continue
 
             read_id = read.query_name
 
             if read_id not in reads_data:
                 reads_data[read_id] = ReadData(read_id)
+            
+            if read.is_supplementary:
+                hard_clipped = read.cigartuples[-1][1]
+            else:
+                hard_clipped = 0
+
+            query_len = read.query_length + hard_clipped
+            if read.is_reverse:
+                query_alignment_start = query_len - read.query_alignment_end
+                query_alignment_end = query_len - read.query_alignment_start
+            else:
+                query_alignment_start = read.query_alignment_start
+                query_alignment_end = read.query_alignment_end
 
             alignment_info = AlignmentInfo(
                 reference_name=self.mapped_bam.get_reference_name(read.reference_id),
                 reference_start=read.reference_start,
                 reference_end=read.reference_end,
-                query_alignment_start=read.query_alignment_start,
-                query_alignment_end=read.query_alignment_end,
+                query_alignment_start=query_alignment_start,
+                query_alignment_end=query_alignment_end,
+                length = read.reference_end - read.reference_start,
                 cigar=read.cigarstring,
                 mapping_quality=read.mapping_quality,
                 flag=read.flag,
-                methylation_data=self._extract_methylation_data(read_id)
+                methylation_data=self.methylation_data_by_read_id.get(read_id, [])
             )
             reads_data[read_id].add_alignment(alignment_info)
 
         return reads_data
 
+def methylation_per_coding():
+    #dorado_bam_path = "test_files/201020_47/calls_2025-02-05_T09-56-59.bam"
+    #mapped_bam_path = "test_files/201020_47_coding_mapped.bam"
+    dorado_bam_path = "test_files/220705_Y9993fN/calls_2025-02-05_T07-37-34.bam"
+    mapped_bam_path = "test_files/220705_Y9993fN_coding_mapped.bam"
+    with BamAnalyzer(mapped_bam_path, dorado_bam_path) as analyzer:
+      reads_data = analyzer.analyze()
+    ave_met_scores = []
+    for read_id, read_data in reads_data.items():
+        for alignment in read_data.alignments:
+            met_list = alignment.methylation_data
+            in_met_scores = []
+            if 4000 < alignment.length < 16000:
+                for i in met_list:
+                    if alignment.query_alignment_start < i[0] < alignment.query_alignment_end:
+                        in_met_scores.append(i[1])
+            if in_met_scores:
+                ave_met_scores.append(np.mean(in_met_scores))
+    plt.hist(ave_met_scores, bins=100)
+    plt.show()
+
+
 def main():
-    mapped_bam_path = "path/to/your/mapped.bam"
-    dorado_bam_path = "path/to/your/dorado.bam"
+    methylation_per_coding()
+    quit()
+    dorado_bam_path = "test_files/201020_47/calls_2025-02-05_T09-56-59.bam"
+    mapped_bam_path = "test_files/201020_47_coding_mapped.bam"
 
     with BamAnalyzer(mapped_bam_path, dorado_bam_path) as analyzer:
       reads_data = analyzer.analyze()
